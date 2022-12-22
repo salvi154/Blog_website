b2300ecc181244c707aa8ede2bda856084195c13
Salvi <salvityagi154@gmail.com>
2022-12-22 17:24:31 +0530

    change port 2

--- app.js
+++ app.js
@@ -84,7 +84,10 @@ app.get('/posts/:postId',function(req,res){
   
 });
 
-
-app.listen(process.env.PORT || 3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
